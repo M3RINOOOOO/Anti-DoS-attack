eb5d9c701ba14b50c74743d194b821ae6d7bb3f4
Manuel Vicente Bolaños Quesada <manbolq@coreo.ugr.es>
2024-05-06 20:21:21 +0200

    Archivo de configuracion con variables globales

---
+++ config.py
@@ -0,0 +1,17 @@
+# Archivo de configuracion
+
+# Variables globales por defecto
+################## RUTA SERVIDOR WEB ##################
+WEB_SERVER_PATH = "/var/www/html/"
+
+################## APACHE ##################
+APACHE_LOG_PATHS = ["/var/log/apache2/access.log",
+                    "/var/log/apache/access.log",
+                    "/var/log/httpd/access.log",
+                    "/var/log/httpd/access_log",
+                    "/var/log/httpd-access.log"]
+APACHE_BAN_PATH = WEB_SERVER_PATH + ".htaccess"
+
+################## NGINX ##################
+NGINX_LOG_PATHS = ["/var/log/nginx/access.log"]
+NGINX_BAN_PATH = "/etc/nginx/sites-available/default"
