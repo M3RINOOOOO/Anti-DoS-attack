6069ae1c3556cef5bc728b20adc2919feb1a7852
Manuel Vicente Bolaños Quesada <manbolq@coreo.ugr.es>
2024-05-07 13:00:00 +0200

    Funcion de monitorizacion basica creada

--- AntiDOSWeb.py
+++ AntiDOSWeb.py
@@ -1,17 +1,22 @@
 import os
 import re
 import subprocess
+from datetime import datetime
 
 
 class AntiDOSWeb:
-    def __init__(self, filename):
+    def __init__(self, filename, formato_fecha):
         self.filename = filename
+        self.formato_fecha = formato_fecha
         self.ult_mod = os.stat(filename).st_mtime
 
     def leer_registros(self):
         try:
-            comando = f"tail -n 20 {self.filename}"
-            registros = subprocess.check_output(comando, shell=True, universal_newlines=True)
+            with open(self.filename, "r") as log:
+                registros = log.read()
+
+            # comando = f"tail -n 20 {self.filename}"
+            # registros = subprocess.check_output(comando, shell=True, universal_newlines=True)
             return registros
         except Exception as e:
             return f"Error al leer los registros: {e}"
@@ -23,9 +28,26 @@ class AntiDOSWeb:
         matches = re.findall(patron, registros)
         for match in matches:
             ip = match[0]
-            hora = match[1]
+            #hora = match[1]
+            hora = datetime.strptime(match[1], self.formato_fecha).timestamp()
+            # if ip in ips_horas:
+            #     ips_horas[ip].append(hora)
+            # else:
+            #     ips_horas[ip] = [hora]
+
             if ip in ips_horas:
-                ips_horas[ip].append(hora)
+                if hora in ips_horas[ip]:
+                    ips_horas[ip][hora] += 1
+                else:
+                    ips_horas[ip][hora] = 1
             else:
-                ips_horas[ip] = [hora]
+                ips_horas[ip] = {hora: 1}
+
         return ips_horas
+
+    def monitor(self):
+        while True:
+            ult_mod_actual = os.stat(self.filename).st_mtime
+            if ult_mod_actual != self.ult_mod:
+                self.ult_mod = ult_mod_actual
+                print(self.extraer_ips_y_horas())
--- DoSTesterCLI.py
+++ DoSTesterCLI.py
@@ -1,9 +1,10 @@
-import subprocess
-import re
 import AntiDOSWeb
 
 if __name__ == "__main__":
 
-    anti_dos = AntiDOSWeb.AntiDOSWeb("/var/log/apache2/access.log")
-    ips_peticiones = anti_dos.extraer_ips_y_horas()
-    print(ips_peticiones)
+    anti_dos = AntiDOSWeb.AntiDOSWeb("/var/log/apache2/access.log", "%d/%b/%Y:%H:%M:%S %z")
+    #ips_peticiones = anti_dos.extraer_ips_y_horas()
+    #print(ips_peticiones)
+
+    anti_dos.monitor()
+
