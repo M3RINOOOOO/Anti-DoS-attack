0b4e264c33079b560bcd840edc42b6d4b87af66e
Manuel Vicente Bolaños Quesada <manbolq@coreo.ugr.es>
2024-05-07 11:44:40 +0200

    Eliminadas las funciones que ahora estan en la clase AntiDOSWeb

--- DoSTesterCLI.py
+++ DoSTesterCLI.py
@@ -1,29 +1,9 @@
 import subprocess
 import re
-
-def leer_registros_apache():
-    try:
-        comando = "tail -n 20 /var/log/apache2/access.log"  
-        registros = subprocess.check_output(comando, shell=True, universal_newlines=True)
-        return registros
-    except Exception as e:
-        return f"Error al leer los registros de Apache2: {e}"
-
-def extraer_ips_y_horas(registros):
-    ips_horas = {}
-    patron = r'(\b(?:\d{1,3}\.){3}\d{1,3}\b) .* \[(.*?)\]'
-    matches = re.findall(patron, registros)
-    for match in matches:
-        ip = match[0]
-        hora = match[1]
-        if ip in ips_horas:
-            ips_horas[ip].append(hora)
-        else:
-            ips_horas[ip] = [hora]
-    return ips_horas
-
+import AntiDOSWeb
 
 if __name__ == "__main__":
-    registros = leer_registros_apache()
-    ips_peticiones = extraer_ips_y_horas(registros)
+
+    anti_dos = AntiDOSWeb.AntiDOSWeb("/var/log/apache2/access.log")
+    ips_peticiones = anti_dos.extraer_ips_y_horas()
     print(ips_peticiones)
