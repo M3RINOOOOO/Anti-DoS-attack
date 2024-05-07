8a3a7f326b62dec56bb7daee5494b2316034c7e6
Cristóbal <cristobal.merino.saez@gmail.com>
2024-05-07 14:14:16 +0200

    Limpieza de archivos y preparación para sqlite

--- AntiDOSWeb.py
+++ AntiDOSWeb.py
@@ -1,25 +1,23 @@
 import os
 import re
-import subprocess
 import config
 from datetime import datetime
 
 
 class AntiDOSWeb:
-    def __init__(self, log_path, formato_fecha, ban_path):
+    def __init__(self, log_path, ban_path, formato_fecha, sqlite_path):
         self.log_path = log_path
         self.formato_fecha = formato_fecha
         self.ult_mod = os.stat(log_path).st_mtime
         self.ban_path = ban_path
         self.ips_horas = {}
+        self.ip_baneadas = []
 
     def leerRegistros(self):
         try:
             with open(self.log_path, "r") as log:
                 registros = log.read()
 
-            # comando = f"tail -n 20 {self.log_path}"
-            # registros = subprocess.check_output(comando, shell=True, universal_newlines=True)
             return registros
         except Exception as e:
             return f"Error al leer los registros: {e}"
@@ -31,12 +29,7 @@ class AntiDOSWeb:
         matches = re.findall(patron, registros)
         for match in matches:
             ip = match[0]
-            #hora = match[1]
             hora = datetime.strptime(match[1], self.formato_fecha).timestamp()
-            # if ip in ips_horas:
-            #     ips_horas[ip].append(hora)
-            # else:
-            #     ips_horas[ip] = [hora]
 
             if ip in ips_horas:
                 if hora in ips_horas[ip]:
@@ -51,7 +44,8 @@ class AntiDOSWeb:
     def banearIp(self, ip):
         try:
             with open(self.ban_path, "a") as ban_file:
-                ban_file.write(f"Deny from {ip}\n")
+                if not f"Deny from {ip}" in open(self.ban_path).read():
+                    ban_file.write(f"Deny from {ip}\n")
             return f"La IP {ip} ha sido baneada"
         except Exception as e:
             return f"Error al banear la IP {ip}: {e}"
@@ -61,9 +55,7 @@ class AntiDOSWeb:
             for hora, peticiones in horas.items():
                 if peticiones > config.MAX_REQUESTS_PER_SEG:
                     self.banearIp(ip)
-                    print(f"La IP {ip} ha sido baneada por exceso de peticiones")
 
-# BANEAR IP
     def monitor(self):
         while True:
             ult_mod_actual = os.stat(self.log_path).st_mtime
@@ -71,4 +63,3 @@ class AntiDOSWeb:
                 self.ult_mod = ult_mod_actual
                 self.extraerIpsHoras()
                 self.checkDos()
-                #print(self.ips_horas)
--- DoSTesterCLI.py
+++ DoSTesterCLI.py
@@ -2,9 +2,7 @@ import AntiDOSWeb
 import config
 if __name__ == "__main__":
 
-    anti_dos = AntiDOSWeb.AntiDOSWeb("/var/log/apache2/access.log", "%d/%b/%Y:%H:%M:%S %z",config.APACHE_BAN_PATH)
-    #ips_peticiones = anti_dos.extraer_ips_y_horas()
-    #print(ips_peticiones)
+    anti_dos = AntiDOSWeb.AntiDOSWeb("/var/log/apache2/access.log", config.APACHE_BAN_PATH, "%d/%b/%Y:%H:%M:%S %z")
 
     anti_dos.monitor()
 
